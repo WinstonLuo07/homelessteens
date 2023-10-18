dfd2629e7a692607d5a31ce1084d3d1d0b9f7c5e
s-wluo <s-wluo@lwsd.org>
2023-10-17 17:23:17 -0700

    empty

---
+++ homelessteens/lib/heartpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Heartpage extends StatefulWidget {
+  const Heartpage({ Key? key }) : super(key: key);
+
+  @override
+  _HeartpageState createState() => _HeartpageState();
+}
+
+class _HeartpageState extends State<Heartpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
--- homelessteens/lib/main.dart
+++ homelessteens/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:homelessteens/heartpage.dart';
+import 'widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -49,6 +51,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  PageController d = PageController();
 
   void _incrementCounter() {
     setState(() {
@@ -95,12 +98,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            // const Text(
+            //   'You have pushed the button this many times:',
+            // ),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
+            PageView(
+              controller: d,
+              children: [
+                Heartpage()
+              ],
             ),
           ],
         ),
